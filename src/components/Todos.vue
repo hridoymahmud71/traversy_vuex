e585e77f69ff4c8981106483516a1a6ee8b89c60
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-12-18 12:06:56 +0600

    finish

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-bind:key="todo.id" v-for="todo in allTodos" class="todo">{{ todo.title }}</div>
+      <div
+        @dblclick="onDblClick(todo)"
+        v-bind:key="todo.id"
+        v-for="todo in allTodos"
+        v-bind:class="{'is-complete':todo.completed}"
+        class="todo"
+      >
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +31,27 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos"])
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+      this.updateTodo(updTodo);
+    }
   },
   computed: mapGetters(["allTodos"]),
-  created(){
+  created() {
     this.fetchTodos();
+  },
+  metaInfo: {
+    title: process.env.VUE_APP_TITLE,
+    meta: [
+      { name: "description", content: "Vuex traversy" },
+      { name: "keywords", content: "vue,traversy,vuex,hridoymahmud71" },
+      { name: "url", content: process.env.VUE_APP_URL }
+    ]
   }
 };
 </script>
@@ -28,6 +62,7 @@ export default {
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 1rem;
 }
+
 .todo {
   border: 1px solid #ccc;
   background: #41b883;
@@ -36,5 +71,40 @@ export default {
   text-align: center;
   position: relative;
   cursor: pointer;
+}
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: crimson;
+  cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #ffffff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,25 +13,53 @@ const getters = {
 const actions = {
     async fetchTodos({ commit }) {
 
-        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        const response = await axios.get(process.env.VUE_APP_JP_BASE_API_URL);
         window.console.log(response);
         window.console.log(commit);
         commit('setTodos', response.data);
     },
     async addTodo({ commit }, title) {
 
-        const response = await axios.post('https://jsonplaceholder.typicode.com/todos',
+        const response = await axios.post(process.env.VUE_APP_JP_BASE_API_URL,
             { title, completed: false });
         window.console.log(response);
         window.console.log(commit);
         commit('newTodo', response.data);
-    }
+    },
+    async deleteTodo({ commit }, id) {
+        const response = await axios.delete(`${process.env.VUE_APP_JP_BASE_API_URL}/${id}`);
+        window.console.log(response);
+        window.console.log(commit);
+        commit('removeTodo', id);
+    },
+    async filterTodos({ commit }, e) {
+        //get selected number
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        const response = await axios.get(`${process.env.VUE_APP_JP_BASE_API_URL}/?_limit=${limit}`);
+        window.console.log(response);
+        window.console.log(commit);
+        commit('setTodos', response.data);
+    },
+    async updateTodo({ commit }, updTodo) {
+        const response = await axios.put(`${process.env.VUE_APP_JP_BASE_API_URL}/${updTodo.id}`, updTodo);
+        window.console.log(response);
+        window.console.log(commit);
+        commit('updTodo', response.data);
+    },
 };
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
-    newTodo: (state, todo) => (state.todos.unshift(todo))
+    newTodo: (state, todo) => (state.todos.unshift(todo)),
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updTodo: (state, updTodo) => {
+        const idx = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if (idx !== -1) {
+            state.todos.splice(idx, 1, updTodo);
+        }
+    }
 };
+
 
 
 export default {
